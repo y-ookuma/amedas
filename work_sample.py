4fbf8cefeceb2d352c1de1b7f2873f013221d5da
y-ookuma <ysk.bigbear@gmail.com>
2024-03-20 20:17:31 +0900

    Update work_sample.py

--- work_sample.py
+++ work_sample.py
@@ -1,6 +1,10 @@
 from get_amedas import Amedas
 
-amedas=Amedas()
+start_day="2024-03-01"
+end_day="2024-03-02"
 
-amedas.get_1by1()
-#amedas.get_bulk()
+amedas=Amedas(lib="ame_master.csv",start_day=start_day,end_day=end_day)
+# station_id　熊谷,さいたま
+station_id=[47626,363] 
+amedas.get_1by1(output_csv=True,station_id)
+#amedas.get_bulk(output_csv=True)
